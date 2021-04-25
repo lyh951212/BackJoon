7d3074d0fe1bca86266da0c618bc5a6b190f5815
DESKTOP-I9D7NLQ\YEONHEELEE <lyh951212@gmail.com>
2021-04-25 23:33:58 +0900

    basic_stage

--- basic_stage/1330.py
+++ basic_stage/1330.py
@@ -1,12 +1,5 @@
-score = (int)input()
-
-if score >= 90 && score <=100:
-    print("A")
-elif score >= 80 && scroe <= 89:
-    print("B")
-elif score >= 70 && scroe <= 79:
-    print("C")
-elif score >= 60 && scroe <= 69:
-    print("D")
-else:
-    print("F")
+a, b = map(int, input().split())
+ 
+if(a > b):  print(">")
+elif(a == b):   print("==")
+else:   print("<")
