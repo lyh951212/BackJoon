d3272bf7e0ccb0391f69fcb6bbefacfc3d969726
DESKTOP-I9D7NLQ\YEONHEELEE <lyh951212@gmail.com>
2022-02-01 20:21:08 +0900

    dp

--- 2579.py
+++ 2579.py
@@ -1,40 +1,22 @@
 import sys
+
 n = int(sys.stdin.readline().rstrip())
 
-if n > 300:
+if n > 300 or n <= 0:
     sys.exit()
     
-floor = [0 for _ in range(n+2)]
-dp = [0 for _ in range(n+2)]
+floor   = [0 for _ in range(301)]
+dp      = [0 for _ in range(301)]
 for i in range(n):
     floor[i+1] = int(sys.stdin.readline().rstrip())
-    
-if sum(floor) > 10000:
-    sys.exit()
-    
+    if floor[i+1] > 10000:
+        sys.exit()
+
 dp_floor_list = [[] for _ in range(n+1)]
-
 dp[1] = floor[1]
-dp_floor_list[1].append(1)
-        
-for i in range(2,n+1):
-    invalid_floor = -1
-    if i-1 in dp_floor_list[i-1] and i-2 in dp_floor_list[i-1]:
-        invalid_floor = i-1
-        
-    if i-1 in dp_floor_list[i-2] and i-2 in dp_floor_list[i-2]:
-        invalid_floor = i-2
-    
-    if -1 != invalid_floor:
-        temp = i-1 if floor[i-1] > floor[i-2] else i-2
-        dp[i] = floor[temp] + floor[i]
-        dp_floor_list[i].append(temp)
-    else:
-        dp_idx = i-1 if dp[i-1] > dp[i-2] else i-2
-        dp[i] = dp[dp_idx] + floor[i]
-        dp_floor_list[i] = dp_floor_list[i] + dp_floor_list[dp_idx]
-        
-    dp_floor_list[i].append(i)
-        
+dp[2] = floor[1] + floor[2]
+dp[3] = max(floor[1] + floor[3], floor[2] + floor[3])
 
+for i in range(4, n+1):
+    dp[i] = max(dp[i-3] + floor[i-1] + floor[i], dp[i-2] + floor[i])
 print(dp[n])
